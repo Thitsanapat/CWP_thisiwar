c14345df7a7404dfe1229ff26f01c160dc460ce5
Thitsanapat <81551627+Thitsanapat@users.noreply.github.com>
2026-09-22 13:09:51 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Thitsanapat"
+last_name="Siwarattanan"
+print(first_name+" "+last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name="Thitsanapat"
+last_name="Siwarattanan"
+whole_name=first_name+" "+last_name
+print(whole_name)
