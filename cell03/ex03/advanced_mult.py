54916d7116b14d447d205ac98debad8d31b30c00
Thitsanapat <thitsanapat@gmail.com>
2026-09-23 14:03:17 +0700

    cell03
    ex00010203

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+
+i = 0
+while i <= 10:
+    j = 0
+    line = f"Table de {i}:"
+    while j <= 10:
+        line += f" {i * j}"
+        j += 1
+    print(line)
+    i += 1
